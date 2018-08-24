4561a783a451d7370f84af12245751fb648925a8
neo <nexoimy@gmail.com>
2018-08-24 13:03:25 +0800

    reset current_num

--- lib/csys/course/courses_dao.ex
+++ lib/csys/course/courses_dao.ex
@@ -152,6 +152,10 @@ defmodule CSys.CourseDao do
     |> where(code: ^code)
     |> Repo.update_all(set: [name: name])
   end
+  def update_course_rest() do
+    Course
+    |> Repo.update_all(set: [current_num: 0])
+  end
 
   #### 退选课 ####
   @doc """
